45c9f3c0bd30454701959bd020fbffe4ca8d6058
midCenturyDude7 <mgriffes@gmail.com>
2024-04-10 21:26:11 -0700

    Set up scaffolding for Header component

--- app/page.js
+++ app/page.js
@@ -1,7 +1,8 @@
-import Image from "next/image";
-
+import Header from "./components/Header";
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24"></div>
+    <div className="bg-white">
+      <Header />
+    </div>
   );
 }
